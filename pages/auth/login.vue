e8f4aab25a44f83c485304dbe36d7aeb68d85829
phojie rengel <phojrengel@gmail.com>
2023-09-13 14:47:07 +0800

    chore(auth): initial auth pages

---
+++ pages/auth/login.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+definePageMeta({
+  title: 'Login',
+  description: 'Login to your account',
+  path: '/login',
+})
+</script>
+
+<template>
+  <main class="mx-auto max-w-lg px-4 lg:px-8 sm:px-6">
+    <div class="mx-auto max-w-xl rounded-md text-center space-y-8">
+      <div class="flex justify-center">
+        <AppLogo class="h-35" />
+      </div>
+
+      <div class="space-y-1">
+        <h1 class="text-3xl font-extrabold text-muted">
+          Sign in to your account
+        </h1>
+        <p class="text-sm font-medium text-primary">
+          Powered by GoTeam Technologies
+        </p>
+      </div>
+
+      <FormLogin />
+
+      <div class="border border-base rounded-md p-4 text-center text-sm">
+        No account yet?
+        <NLink btn="link square" to="/signup">
+          Create an account here
+        </NLink>
+      </div>
+    </div>
+  </main>
+</template>
---
+++ pages/auth/signup.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+definePageMeta({
+  title: 'Sign Up',
+  description: 'Sign up for a new account',
+  path: '/signup',
+})
+</script>
+
+<template>
+  <div>
+    test
+  </div>
+</template>
