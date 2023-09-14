7215ab352df1e120687d97a65a5c2f0a9fd29b39
phojie rengel <phojrengel@gmail.com>
2023-09-14 12:11:31 +0800

    refactor(useValidation): separate the focus fn to make reusable outside the composable

--- composables/useValidation.ts
+++ composables/useValidation.ts
@@ -1,15 +1,20 @@
 import { z } from 'zod'
 
 // schema: any for now, but should be zod schema
-export function useValidation(schema: any, form: Record<string, any>) {
+export function useValidation(form: Record<string, any>, schema: any) {
   type Form = z.infer<typeof schema>
-  const errors = ref<Record<keyof Form, any>>({})
+  const errors = ref()
   const formValue = toValue(form)
+
   // validate form on submit
   function validateForm(isFocused = true) {
     try {
       errors.value = {}
       schema.parse(formValue)
+
+      // if no errors
+      if (typeof errors.value === 'object' && Object.keys(errors.value).length === 0)
+        return true
     }
     catch (err) {
       if (err instanceof z.ZodError)
@@ -21,21 +26,23 @@ export function useValidation(schema: any, form: Record<string, any>) {
       // focus on the first error, get the key of the first error
       const firstError = Object.keys(errors.value)[0] as keyof Form
       if (!firstError)
-        return
+        return false
 
-      const refId = document.getElementById(firstError as string)
-      refId?.focus()
+      // focus on the first error
+      focusInput(firstError)
+
+      return false
     }
   }
 
   // validate field on blur
-  function validateField(field: { id: keyof Form; value: string }) {
+  function validateField(id: keyof Form) {
     try {
       errors.value = errors.value && Object.fromEntries(
-        Object.entries(errors.value).filter(([key]) => key !== field.id),
+        Object.entries(errors.value).filter(([key]) => key !== id),
       )
 
-      schema.pick({ [field.id]: true }).parse({ [field.id]: field.value })
+      schema.pick({ [id]: true }).parse({ [id]: formValue[id as string] })
     }
     catch (err) {
       if (err instanceof z.ZodError)
@@ -43,5 +50,13 @@ export function useValidation(schema: any, form: Record<string, any>) {
     }
   }
 
-  return { validateForm, validateField, errors }
+  // focus on input
+  function focusInput(id: keyof Form) {
+    nextTick(() => {
+      const refId = document.getElementById('password')
+      refId?.focus()
+    })
+  }
+
+  return { validateForm, validateField, focusInput, errors }
 }
