6dd2ad981d15ad6eabad2530bc969bf45866cb56
phojie rengel <phojrengel@gmail.com>
2023-09-16 05:25:50 +0800

    chore: update footer layout

--- components/App/AppFooter.vue
+++ components/App/AppFooter.vue
@@ -3,8 +3,8 @@ const { name } = useAppInfo()
 </script>
 
 <template>
-  <footer class="mx-auto mt-40 max-w-7xl overflow-hidden px-6 pb-20 sm:mt-64 lg:px-8 sm:pb-24">
-    <p class="mt-10 text-center text-xs leading-5 text-muted">
+  <footer class="mx-auto overflow-hidden px-6 container lg:px-8">
+    <p class="text-center text-xs leading-5 text-muted">
       &copy; 2023 {{ name }}. All rights reserved.
     </p>
   </footer>
