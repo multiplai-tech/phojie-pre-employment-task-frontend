bd36fb9199e1cd06bd6188b8c5368bab09867fb2
phojie rengel <phojrengel@gmail.com>
2023-09-15 22:29:05 +0800

    chore(wip): header

--- components/App/AppHeader.vue
+++ components/App/AppHeader.vue
@@ -1,39 +1,46 @@
 <script setup lang="ts">
 const colorMode = useColorMode()
+
+const { user } = useAuthStore()
 </script>
 
 <template>
-  <div class="grid grid-cols-6 h-16 border-b border-base px-4 shadow-sm space-x-1 lg:px-8 md:px-6">
+  <header class="fixed left-0 right-0 top-0 z-10 grid grid-cols-6 h-18 border-b border-base bg-base px-4 shadow-sm space-x-1 lg:px-8 md:px-6">
     <nav class="col-span-2 flex items-center lg:space-x-20">
       <!-- left -->
-      <div class="min-w-30 w-8 w-auto">
-        <img :src=" colorMode.preference === 'dark' ? '/goteam-og-light.png' : '/goteam-og.png'" class="h-8">
+      <div class="h-8">
+        <ClientOnly>
+          <img :src=" colorMode.preference === 'dark' ? '/goteam-og-light.png' : '/goteam-og.png'" class="h-8">
+        </ClientOnly>
       </div>
 
       <!-- tabs -->
-      <div class="hidden flex-nowrap items-center text-sm lg:flex space-x-4">
-        <NNavLink btn="text-gray" label="Careers" :una="{ btn: 'font-medium' }" />
-        <NNavLink btn="text-gray" label="Find your role" :una="{ btn: 'font-medium' }" />
+      <div class="hidden shrink-0 flex-nowrap items-center lg:flex space-x-4">
+        <NNavLink label="Careers" :una="{ btn: 'text-$c-gray-500 font-normal !bg-transparent' }" />
+        <NNavLink label="Find your role" :una="{ btn: 'text-$c-gray-500 font-normal !bg-transparent' }" />
       </div>
     </nav>
 
     <!-- center -->
     <div class="col-span-2 flex items-center">
       <NInput
-        class="rounded-full py-2 pl-5 pr-25 sm:pr-30"
+        class="rounded-full py-2.5 pl-6 pr-25 text-md sm:pr-30 placeholder:font-light placeholder:text-$c-gray-400"
         placeholder="Type for job title or role"
         :una="{
           inputWrapper: 'grow px-1.5 hidden sm:flex',
-          inputTrailing: 'pointer-events-auto cursor-pointer',
+          inputTrailingWrapper: 'pointer-events-auto cursor-pointer',
         }"
       >
         <template #trailing>
           <!-- search icon -->
           <NButton
-            label="Search"
-            class="rounded-full py-1.5 text-xs font-light shadow-none sm:px-4"
+            class="rounded-full py-1.6 shadow-none lg:px-4"
             leading="i-heroicons-magnifying-glass-20-solid"
-          />
+          >
+            <span class="hidden text-xs font-light tracking-wide lg:block">
+              Search
+            </span>
+          </NButton>
         </template>
       </NInput>
     </div>
@@ -43,11 +50,11 @@ const colorMode = useColorMode()
       <div class="flex grow items-center justify-end">
         <div class="group ml-10 flex cursor-pointer items-center space-x-2.5">
           <div class="text-right text-xs leading-4">
-            <div class="font-medium">
-              Reyven Biloy
+            <div class="font-bold">
+              {{ user?.name }}
             </div>
             <div class="text-primary">
-              reyvenb@gmail.com
+              {{ user?.email }}
             </div>
           </div>
 
@@ -64,5 +71,5 @@ const colorMode = useColorMode()
         </div>
       </div>
     </div>
-  </div>
+  </header>
 </template>
