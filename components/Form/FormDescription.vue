b28a3d913573f6870d5c13c20a9392e772e7a89a
phojie rengel <phojrengel@gmail.com>
2023-09-16 04:54:27 +0800

    feat(component): add form-description

---
+++ components/Form/FormDescription.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+interface Description {
+  label: string
+  value: string
+  ddClass?: string
+}
+
+interface ProfileDetailsProps {
+  descriptions: Description[]
+  title: string
+}
+
+defineProps<ProfileDetailsProps>()
+</script>
+
+<template>
+  <div>
+    <div class="flex justify-between pl-7 pr-5">
+      <h3 class="font-semibold leading-7 text-base text-gray-900">
+        {{ title }}
+      </h3>
+
+      <!-- profile actions -->
+      <NButton
+        size="xs"
+        btn="text-gray"
+        class="opacity-70 hover:opacity-100"
+        icon
+        label="i-heroicons-pencil"
+      />
+    </div>
+
+    <!-- description list -->
+    <dl class="px-7">
+      <div v-for="(item, index) in descriptions" :key="index" class="py-2 sm:grid sm:grid-cols-3">
+        <dt class="text-13px font-light leading-6 text-$c-gray-500">
+          {{ item.label }}
+        </dt>
+        <dd
+          class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0"
+          :class="item.ddClass"
+        >
+          {{ item.value }}
+        </dd>
+      </div>
+    </dl>
+  </div>
+</template>
