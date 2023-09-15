16066f256f5bc0e2771d619407dacbdac051e4cb
phojie rengel <phojrengel@gmail.com>
2023-09-16 05:27:13 +0800

    feat(component): add new profile component

---
+++ components/Profile/ProfilePreferences.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const descriptions = [
+  { label: 'Day Shift', value: '-' },
+  { label: 'Mid Shift', value: '-' },
+  { label: 'Night Shift', value: '-' },
+  { label: 'English Only Policy (EOP)', value: '-' },
+]
+</script>
+
+<template>
+  <FormDescription
+    :descriptions="descriptions"
+    title="Preferences"
+    class="py-6"
+  />
+</template>
