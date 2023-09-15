973a9300589fef44535d1c87b1cc6a3cad81463a
phojie rengel <phojrengel@gmail.com>
2023-09-16 05:27:04 +0800

    feat(component): add new profile component

---
+++ components/Profile/ProfileDetails.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const descriptions = [
+  { label: 'Date of Birth', value: '07 July 1986' },
+  { label: 'Gender', value: 'Male' },
+  { label: 'Civil Status', value: 'Married' },
+  { label: 'Nationality', value: 'Filipino' },
+]
+</script>
+
+<template>
+  <FormDescription
+    :descriptions="descriptions"
+    title="General Details"
+    class="py-6"
+  />
+</template>
