cf6c5e0093a426633f982c93ca487f8b52998710
phojie rengel <phojrengel@gmail.com>
2023-09-17 10:14:08 +0800

    fix: config cors properly

--- composables/fetch.ts
+++ composables/fetch.ts
@@ -21,7 +21,7 @@ export function useFetchApi<T>(path: string, options: UseFetchOptions<T> = {}) {
     }
   }
 
-  path = path.startsWith('/') ? backendUrl + apiBase + path : path
+  path = path.startsWith('/') ? `/api${path}` : path
 
   return useFetch(path, {
     credentials: 'include',
@@ -35,9 +35,5 @@ export function useFetchApi<T>(path: string, options: UseFetchOptions<T> = {}) {
 }
 
 export async function useFetchCsrf<T>(path?: string, options: UseFetchOptions<T> = {}) {
-  const { backendUrl } = useRuntimeConfig().public
-
-  path = path || `${backendUrl}/sanctum/csrf-cookie`
-
-  return await useFetchApi(path, options)
+  return await useFetch('/sanctum/csrf-cookie')
 }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,6 +17,10 @@ export default defineNuxtConfig({
   routeRules: {
     // '/': { prerender: true },
     '/tasks/**': { ssr: false },
+    // eslint-disable-next-line n/prefer-global/process
+    '/api/**': { proxy: { to: `${process.env.NUXT_PUBLIC_API_URL}/api/v1/**` }, cors: true },
+    // eslint-disable-next-line n/prefer-global/process
+    '/sanctum/csrf-cookie': { proxy: { to: `${process.env.NUXT_PUBLIC_API_URL}/sanctum/csrf-cookie` }, cors: true },
   },
 
   runtimeConfig: {
