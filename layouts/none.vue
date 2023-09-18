01fbdf83d3d0f47662740d60e47b00287aef39d4
phojie rengel <phojrengel@gmail.com>
2023-09-18 19:51:25 +0800

    chore: display build and env status at none layout

--- layouts/none.vue
+++ layouts/none.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+const { version, env } = useBuildInfo()
+</script>
+
 <template>
   <div>
+    <em class="absolute right-10 top-10 text-xs font-bold text-base">v{{ version }} ({{ env }})</em>
     <slot />
   </div>
 </template>
